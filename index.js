5a30d9ed2c733eb80d02316235df7f3b82d63d9e
Alexander Strutz <alexander.strutz@able-group.de>
2021-05-25 14:30:31 +0200

    Using Auth Token for HomeApp-Router

--- index.js
+++ index.js
@@ -3,17 +3,18 @@ const axios = require('axios');
 /**
  * Checks if a user is in a specific group
  * @param {Object} req Express.js request object
- * @param {String} adminGroup Group to be checked
+ * @param {String} group Group to be checked
  * @returns {Boolean} Indicates if a user is in the group
  */
 async function load (req, group) {
 	let isInGroup = false;
-	if(req.headers.cookie) {
+	if(req.headers.cookie || req.headers.Authorization || req.headers.authorization) {
 		const options = {
 			headers: {
 				Accept: 'application/hal+json',
 				'Content-Type': 'application/hal+json',
-				Cookie: req.headers.cookie,
+				Cookie: req.headers.cookie || '',
+				Authorization: req.headers.Authorization || req.headers.authorization || '',
 			},
 			url: `${req.systemBaseUri}/identityprovider/validate?allowExternalValidation=true`,
 		};
